fa3d06674446f72ae66c37e7e488fa75f994b5be
Mark Revin Fragata <115454281+patrickstarhasb00bs84@users.noreply.github.com>
2024-07-21 18:22:48 +0800

    DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -1,57 +1,58 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+ // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "@openzeppelin/contracts@4.9.0/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
-
+// This contract implements the ERC20 standard for tokens and includes the Ownable contract from OpenZeppelin for access control.
 contract DegenToken is ERC20, Ownable {
 
-    enum Character {Pikachu, Bulbasaur, Charmander}
+// The redemption cost for magic is set to 100 tokens per unit of magic.
+uint256 public constant REDEMPTION_COST = 100;
 
-    uint256 constant private POKEMON_COST = 200; 
+// This mapping keeps track of the amount of magic owned by each user.
+mapping(address => uint256) public magicOwned;
 
-    uint256 constant private PIKACHU_CHANCE = 700; 
-    uint256 constant private BULBASAUR_CHANCE = 300;   
-    uint256 constant private CHARMANDER_CHANCE = 25;   
-    mapping(Character => uint256) public inventory;
-    constructor() ERC20("Degen", "DGN") {}
+// The constructor initializes the contract with an initial supply of tokens for the contract creator.
+constructor() 
+    ERC20("Degen", "DGN") Ownable(msg.sender) {
+    _mint(msg.sender, 10 * (10 ** uint256(decimals())));
+}
 
-    function mint(uint256 amount) public onlyOwner {
-        _mint(msg.sender, amount);
-    }
+// This function allows users to redeem their DegenTokens for a "powerful magic" at a cost of 100 tokens per unit of magic.
+// The function burns the tokens and updates the user's magic balance in the `magicOwned` mapping.
+function redeemMagic(uint256 quantity) public {
+    uint256 cost = REDEMPTION_COST * quantity;
+    require(balanceOf(msg.sender) >= cost, "Your token is not enough tokens to redeem for a powerful magic");
 
-    function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
-    }
+    magicOwned[msg.sender] += quantity;
+    _burn(msg.sender, cost);
+}
 
-    function transfer(address to, uint256 amount) public override returns (bool) {
-        _transfer(msg.sender, to, amount);
-        return true;
-    }
+// This function returns the amount of magic owned by the specified user.
+function checkMagicOwned(address user) public view returns (uint256) {
+    return magicOwned[user];
+}
 
-    function pokemon() public payable {
-        require(balanceOf(msg.sender) >= POKEMON_COST, "Insufficient balance for pokemon");
+// This function allows the contract owner to mint new tokens and send them to a specified address.
+function mintTokens(address to, uint256 amount) public onlyOwner {
+    _mint(to, amount);
+}
 
-        _burn(msg.sender, POKEMON_COST);
+// This function returns the balance of tokens owned by the specified address.
+function checkBalance(address account) public view returns (uint256) {
+    return balanceOf(account);
+}
 
-        // Using `block.prevrandao` instead of the deprecated `block.difficulty`
-        uint256 randomNumber = uint256(keccak256(abi.encodePacked(block.timestamp, block.prevrandao, msg.sender))) % 1000;
+// This function allows users to burn their tokens, removing them from circulation.
+function burnTokens(uint256 amount) public {
+    require(balanceOf(msg.sender) >= amount, "Your token is not enough tokens to burn");
+    _burn(msg.sender, amount);
+}
 
-        Character result;
-
-        if (randomNumber < PIKACHU_CHANCE) {
-            result = Character.Pikachu;
-        } else if (randomNumber < PIKACHU_CHANCE + BULBASAUR_CHANCE) {
-            result = Character.Bulbasaur;
-        } else if (randomNumber < PIKACHU_CHANCE + BULBASAUR_CHANCE + CHARMANDER_CHANCE) {
-            result = Character.Charmander;
-        } else {
-            revert("Invalid random number generated");
-        }
-        inventory[result]++;
-    }
-
-    function getInventory() public view returns (uint256, uint256, uint256) {
-        return (inventory[Character.Pikachu], inventory[Character.Bulbasaur], inventory[Character.Charmander]);
-    }
+// This function allows users to transfer their tokens to another address.
+function transferTokens(address to, uint256 amount) public {
+    require(to != address(0), "Your address is invalid ");
+    require(balanceOf(msg.sender) >= amount, "Your token is not enough tokens to transfer");
+    _transfer(msg.sender, to, amount);
+}
 }
